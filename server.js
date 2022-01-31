28feb30a1607eca22fc96068bde3a055b3b07869
Ayoush Moktan Lama <aayushlama26@gmail.com>
2022-01-31 16:09:05 +0545

    env

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: './.env',
+});
+
 const PORT = process.env.PORT || 5000;
 
 const server = require('./src/App')({
--- test/setTestEnv.js
+++ test/setTestEnv.js
@@ -1,3 +1,7 @@
+const DB_USER = 'postgres';
+const DB_PASSWORD = 'ayoush26';
+
 if (!process.env.POSTGRES_URI) {
-  process.env.POSTGRES_URI = 'postgres://app:password@localhost:5432/db';
+  // process.env.POSTGRES_URI = 'postgres://postgres:ayoush26@localhost:5432/db';
+  process.env.POSTGRES_URI = `postgres://${DB_USER}:${DB_PASSWORD}@localhost:5432/db`;
 }
